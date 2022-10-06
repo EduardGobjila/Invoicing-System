44269816301a1d57926d67ffc731a675b7911961
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-06 21:06:11 +0100

    added the forign key to invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ApplicationRecord
   validates :company_name, :price, :paid_with, presence: true
 
-  # belongs_to :users
+  belongs_to :users
 
   CATEGORIES = ['Amber Bakery', 'RoExport', 'Monolith', 'Lithuanian Beer', 'Asorti Food', 'Marion', 'Cake World', 'Cypro Food',
                  'Imperial', 'Sitella Fruits', 'Ambassador Bakery', 'Dancle Romanian' ].sort
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_223053) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_193539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_223053) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "paid_with"
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_invoices_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_223053) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "invoices", "users", column: "users_id"
 end
