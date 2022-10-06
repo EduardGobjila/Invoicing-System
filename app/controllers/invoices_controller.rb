f4031596526195659b5f95f8dc12b46e9fb63d57
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-06 12:56:37 +0100

    trying to add the logic sorting the invoice per week

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,7 +1,16 @@
 class InvoicesController < ApplicationController
 
   def index
-    @invoices = Invoice.all.reverse
+    if params[:sort] == "current"
+      today = Date.today
+      @invoices = Invoice.all.sort_by { today.at_beginning_of_week..today.at_end_of_week }
+    elsif params[:sort] == "last_week"
+      @invoices = Invoice.order(:created_at)
+    elsif params[:sort] == "oldest"
+      @invoices = Invoice.order(created_at: :asc)
+    else
+      @invoices = Invoice.all.reverse
+    end
   end
 
   def show
