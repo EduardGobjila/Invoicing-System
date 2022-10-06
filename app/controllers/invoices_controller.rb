d4f6917040f3ee84fe094d4f7dc0f210e4d23dcc
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-06 20:01:19 +0100

    added the logic to show all the invoices current week

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,10 +2,9 @@ class InvoicesController < ApplicationController
 
   def index
     if params[:sort] == "current"
-      @invoices_current_week = Invoice.all
-      @invoices = @invoices_current_week.sort_by do
-        Date.today.beginning_of_week..Date.today.end_of_week
-      end
+
+      # @invoices = Invoice.where("created_at >= ?", Date.today.beginning_of_week..Date.today.end_of_week)
+      @invoices = Invoice.where("created_at >= ?", DateTime.now.beginning_of_week)
     elsif params[:sort] == "last_week"
       # @invoices = ActiveRecord::Base.connection.execute(SELECT * FROM invoices WHERE id = 1)
       @invoice = Invcoice.all
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,9 @@
 class Invoice < ApplicationRecord
   validates :company_name, :price, :paid_with, presence: true
 
-  belongs_to :users
+  # belongs_to :users
 
-  CATEGORIES = ['Amber Bakery', 'RoExport', 'Monolith', 'Lithuanian Beer', 'Asorti Food', 'Marion', 'Cake World', 'Cypro Food', 'Imperial', 'Sitella Fruits', 'Ambassador Bakery', 'Dancle Romanian' ].sort
+  CATEGORIES = ['Amber Bakery', 'RoExport', 'Monolith', 'Lithuanian Beer', 'Asorti Food', 'Marion', 'Cake World', 'Cypro Food',
+                 'Imperial', 'Sitella Fruits', 'Ambassador Bakery', 'Dancle Romanian' ].sort
+  PAID = ["Card", "Cash"]
 end
