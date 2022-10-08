1de24fd3aa0aa83c93e2739b3ee910b1c8e17f4b
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-08 11:04:46 +0100

    bug in production

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,6 +5,7 @@ class Invoice < ApplicationRecord
 
   CATEGORIES = ['Amber Bakery', 'RoExport', 'Monolith', 'Lithuanian Beer', 'Asorti Food', 'Marion', 'Cake World', 'Cypro Food',
                  'Imperial', 'Sitella Fruits', 'Ambassador Bakery', 'Dancle Romanian', 'Noble Wine', 'Baltic Foods', 'Ulan',
-                'Lackmann', 'Leon', 'Bills', 'Holland Bazar', 'Tahtakale', 'Rambynas', 'Salary', 'Import', 'Other' ].sort
-  PAID = ["Card", "Cash", '444 Foods', 'Lithuanica']
+                'Lackmann', 'Leon', 'Bills', 'Holland Bazar', 'Tahtakale', 'Rambynas', 'Salary', 'Import', 'Other',
+                'Foods 444', 'Lithuanica' ].sort
+  PAID = ["Card", "Cash"]
 end
