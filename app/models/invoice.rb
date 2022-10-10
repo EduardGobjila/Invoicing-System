197c2183b2b86134d31c24e4c2d12768f641a815
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-10 15:07:33 +0100

    production check

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ApplicationRecord
   validates :company_name, :price, :paid_with, presence: true
 
-  belongs_to :user
+  # belongs_to :user
 
   CATEGORIES = ['Amber Bakery', 'RoExport', 'Monolith', 'Lithuanian Beer', 'Asorti Food', 'Marion', 'Cake World', 'Cypro Food',
                  'Imperial', 'Sitella Fruits', 'Ambassador Bakery', 'Dancle Romanian', 'Noble Wine', 'Baltic Foods', 'Ulan',
