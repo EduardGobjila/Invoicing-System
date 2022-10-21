5188f09dae12758f21067809d444f6f8b711bed8
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-21 13:50:55 +0100

    fixed a bug in production with foreign key

--- db/migrate/20221005183349_invoices.rb
+++ db/migrate/20221005183349_invoices.rb
@@ -1,5 +1,6 @@
 class Invoices < ActiveRecord::Migration[7.0]
   def change
     add_column :invoices, :paid_with, :string
+    add_reference :invoices, :user, foreign_key: true
   end
 end
--- db/migrate/20221005223053_adding_to_invoices.rb
+++
@@ -1,5 +0,0 @@
-class AddingToInvoices < ActiveRecord::Migration[7.0]
-  def create
-      add_foreign_key :invoices, :users
-  end
-end
